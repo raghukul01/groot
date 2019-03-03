bf2e9cac0572cc62145070217b10092a98cf84db
Vibhor <vibhorporwal99@gmail.com>
2019-03-03 22:33:39 +0530

    added worker for grep

--- src/api/getlistener.go
+++ src/api/getlistener.go
@@ -1,7 +1,22 @@
 package api
 
-import "github.com/gorilla/mux"
+import (
+	"github.com/gorilla/mux"
+	"os/exec"
+	"encoding/json"
+)
 
 func addGetApis(router *mux.Router) {
+	x := viper.GetString("env")
+	if x == "WORKER" {
+		// assuming input string is str
+		// file is the file in which str is supposed to be searched
+		node := viper.GetString("NODE_INDEX")
+		file := viper.GetString("PROCESSES[0][\"NAME\"]")
+		cmd := exec.command("grep", str, file)
+		output := cmd.Run()
+		// node and process are node and process index
+		return output, node, file
+	}
 
 }
